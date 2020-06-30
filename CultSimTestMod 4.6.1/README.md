35ecc0c9003897a149f0ec7c4bcaa92d5bd211ca
justastranger <justastrangerlica@gmail.com>
2020-06-29 22:32:06 -0500

    Another commit

---
+++ CultSimTestMod 4.6.1/README.md
@@ -0,0 +1,9 @@
+﻿# fastmodemultiplier
+This was probably the simplest first thing I figured I could make with BepInEx. All it does it create a config file that allows you to increase the rate of time.
+
+# Installation
+Extract `fastmodemultiplier.dll` to your `Cultist Simulator\BepInEx\plugins` folder.
+
+# Usage
+Open `Cultist Simulator\BepInEx\config\justastranger.fastmodemultiplier.cfg` in your favorite text editor.
+Find the line that says `Multiplier = 1` and change that 1 to whatever value you want that's greater than or equal to 1. 
--- CultSimTestMod 4.6.1/fastmodemultiplier.cs
+++ CultSimTestMod 4.6.1/fastmodemultiplier.cs
@@ -15,9 +15,14 @@ namespace fastmodemultiplier
         // stuff to do right away
         void Awake()
         {
-            speedMultiplier = Config.Bind("Speed", "Multiplier", 1.0f, "Speed multiplier for fast mode.");
+            speedMultiplier = Config.Bind("Speed", "Multiplier", 1.0f, "Speed multiplier for fast mode.\nAllowed Values: 1-∞\nBe Reasonable! A too high value will make the game completely unplayable.\nA Multiplier of 10 results in an overall speed of 30 seconds per second.");
             Logger.LogInfo("Configuration Loaded. Speed Multiplier set to: " + speedMultiplier.Value.ToString());
-
+            if (speedMultiplier.Value < 1f)
+            {
+                Logger.LogWarning("Speed Multiplier was less than one, which is not yet supported. Defaulting to 1f.");
+                speedMultiplier.Value = 1f;
+            }
+            
             HarmonyInstance = new Harmony("justastranger.fastmodemultiplier");
             HarmonyInstance.PatchAll();
             Logger.LogInfo("Harmony Patch Applied");
